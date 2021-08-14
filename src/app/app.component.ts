c0ad5623485ef7aaf0f1adb5b0e7a9bca51d4454
Sajin-storm <Aistator@gmail.com>
2021-08-14 11:10:10 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div >
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><h3>Arogya Nikethan</h3></a>
+  
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" routerLink='/welcome'>Home<span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    <div>
+      <a class="nav-link" routerLink= '/login'>Hospital / Admin</a>
+    </div>
+  </div>
+</nav>
+  </div>
+  <pm-welcome></pm-welcome>
+  <!-- <pm-login></pm-login> -->
+  `
+  ,styleUrls : ['./app.component.css']
 })
 export class AppComponent {
   title = 'arogya-nikethan';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent,
   ],
+  
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: WelcomeComponent},
+      {path: 'welcome' , component: WelcomeComponent},
+      {path: 'login', component: LoginComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
