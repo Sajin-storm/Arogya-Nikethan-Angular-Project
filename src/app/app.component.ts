1c9665fa8905c4ebb1c63dbc27398739d33e9195
Sajin-storm <Aistator@gmail.com>
2021-08-14 14:47:33 +0530

    Successful route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,18 @@ import { Component } from '@angular/core';
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" routerLink='/welcome'>Home<span class="sr-only">(current)</span></a>
+        <a class="nav-link" routerLink= "/welcome">Home<span class="sr-only">(current)</span></a>
       </li>
     </ul>
     <div>
-      <a class="nav-link" routerLink= '/login'>Hospital / Admin</a>
+      <a class="nav-link" routerLink= "/login">Hospital / Admin</a>
     </div>
   </div>
 </nav>
   </div>
-  <pm-welcome></pm-welcome>
+  <!-- <pm-welcome></pm-welcome> -->
   <!-- <pm-login></pm-login> -->
+  <router-outlet></router-outlet>
   `
   ,styleUrls : ['./app.component.css']
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'pm-login',
@@ -8,5 +9,7 @@ import { Component } from "@angular/core";
 })
 
 export class LoginComponent{
+
+    constructor(private route: ActivatedRoute, private router: Router) { }
 
 }
